f5b9ebefb9be4d7836232e6fd7185efaeb564758
Davis Gossage <dcg13@duke.edu>
2016-04-04 18:17:48 -0400

    Initial Commit

---
+++ poker-tvTests/poker_tvTests.swift
@@ -0,0 +1,36 @@
+//
+//  poker_tvTests.swift
+//  poker-tvTests
+//
+//  Created by Davis Gossage on 4/4/16.
+//  Copyright © 2016 Davis Gossage. All rights reserved.
+//
+
+import XCTest
+@testable import poker_tv
+
+class poker_tvTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
