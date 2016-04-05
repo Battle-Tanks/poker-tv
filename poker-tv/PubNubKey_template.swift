dd1e96c8c0603130bbf32fd989b81007b32d2320
Davis Gossage <dcg13@duke.edu>
2016-04-04 22:52:43 -0400

    Add pubnub key template

---
+++ poker-tv/PubNubKey_template.swift
@@ -0,0 +1,14 @@
+//
+//  PusherKeys.swift
+//  battle-tanks-sprite
+//
+//  Created by Davis Gossage on 1/2/16.
+//  Copyright © 2016 Davis Gossage. All rights reserved.
+//
+
+import UIKit
+
+class PubNubKey: NSObject {
+    static let publish_key = "pub-c-KEY”
+    static let subscribe_key = "sub-c-KEY”
+}
